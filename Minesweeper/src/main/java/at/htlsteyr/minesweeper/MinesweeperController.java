4c0163231527d6cba949986e23beed061b3910b3
jrodleit <jrodleit@htl-steyr.ac.at>
2024-01-31 11:07:37 +0100

    Bombs fixed

--- Minesweeper/src/main/java/at/htlsteyr/minesweeper/MinesweeperController.java
+++ Minesweeper/src/main/java/at/htlsteyr/minesweeper/MinesweeperController.java
@@ -15,46 +15,51 @@ public class MinesweeperController {
 
     @FXML
     GridPane gridPane = new GridPane();
-    private int j = 0;
-    private int k = 0;
+    private final int j = 0;
+    private final int k = 0;
     private final int MAX = 8;
     private final Button[][] buttons = new Button[MAX][MAX];
     private LinkedList<Bombs> bombList = new LinkedList<Bombs>();
     private ListIterator<Bombs> bombIterator = bombList.listIterator();
 
-
-    private boolean randomBombs;
-
     @FXML
     private void initialize() {
-        for (int j = 0; j < 8; j++) {
-            for (int k = 0; k < 8; k++) {
+        //Setzen der Buttons
+        for (int j = 0; j < MAX; j++) {
+            for (int k = 0; k < MAX; k++) {
                 buttons[j][k] = new Button();
                 buttons[j][k].setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
                 gridPane.add(buttons[j][k], j, k);
-                Random random = new Random();
-                randomBombs = random.nextDouble() < 0.16;
 
                 final int x = j;
                 final int y = k;
 
-                buttons[j][k].setOnAction(event -> buttonClick(event, x, y, randomBombs));
-                buttons[j][k].setOnAction(event -> buttonClick(event, x, y, randomBombs));
+                buttons[j][k].setOnAction(actionEvent -> buttonClick(actionEvent, y, x));
+            }
+        }
 
-                if (randomBombs) {
-                    buttons[j][k].setStyle("-fx-background-color: red");
-                    buttons[j][k].setOnAction(event -> Bombs(event, x, y));
-                    bombList.add(new Bombs(x, y, true));
-                }
+        //Setzen von Bomben
+        Random random = new Random();
+        int bombsToPlace = 10;
+        int bombsPlaced = 0;
+
+        while (bombsPlaced <= bombsToPlace) {
+            int j = random.nextInt(MAX);
+            int k = random.nextInt(MAX);
+
+            if (!bombList.contains(new Bombs(j, k, false))) {
+                buttons[j][k].setStyle("-fx-background-color: red");
+                buttons[j][k].setOnAction(event -> Bombs(event, j, k));
+                bombList.add(new Bombs(j, k, true));
+                bombsPlaced++;
             }
         }
         setNumbers();
     }
 
 
-    private void buttonClick(ActionEvent event, int x, int y, Boolean random) {
+    private void buttonClick(ActionEvent event, int x, int y) {
         System.out.println("Coordinates: (" + x + ", " + y + ")");
-        System.out.println("Random value: " + random);
     }
 
     private void Bombs(ActionEvent event, int x, int y) {
@@ -79,53 +84,43 @@ public class MinesweeperController {
         for (Bombs bomb : bombList) {
             int bombX = bomb.getX();
             int bombY = bomb.getY();
-            int surroundingBombs = 1;
 
-            // Nummern setzen der linken Spalte
-            if (bombX > 0) {
-                buttons[bombX - 1][bombY].setText("1");
-                if (bombY > 0) {
-                    buttons[bombX - 1][bombY - 1].setText("1");
-                }
-                if (bombY < 7) {
-                    buttons[bombX - 1][bombY + 1].setText("1");
-                }
-            }
+            for (int i = 0; i < 3; i++) {
+                for (int l = 0; l < 3; l++) {
+                    // Nummern setzen der linken Spalte
+                    if (bombX > 0) {
+                        buttons[bombX - 1][bombY].setText("1");
+                        if (bombY > 0) {
+                            buttons[bombX - 1][bombY - 1].setText("1");
+                        }
+                        if (bombY < 7) {
+                            buttons[bombX - 1][bombY + 1].setText("1");
+                        }
+                    }
 
-            // Nummern setzen der mittleren Spalte
-            if (bombY > 0) {
-                buttons[bombX][bombY - 1].setText("1");
-            }
-            if (bombY < 7) {
-                buttons[bombX][bombY + 1].setText("1");
-            }
+                    // Nummern setzen der mittleren Spalte
+                    if (bombY > 0) {
+                        buttons[bombX][bombY - 1].setText("1");
+                    }
+                    if (bombY < 7) {
+                        buttons[bombX][bombY + 1].setText("1");
+                    }
 
-            // Nummern setzen der rechten Spalte
-            if (bombX < 7) {
-                buttons[bombX + 1][bombY].setText("1");
-                if (bombY > 0) {
-                    buttons[bombX + 1][bombY - 1].setText("1");
-                }
-                if (bombY < 7) {
-                    buttons[bombX + 1][bombY + 1].setText("1");
+                    // Nummern setzen der rechten Spalte
+                    if (bombX < 7) {
+                        buttons[bombX + 1][bombY].setText("1");
+                        if (bombY > 0) {
+                            buttons[bombX + 1][bombY - 1].setText("1");
+                        }
+                        if (bombY < 7) {
+                            buttons[bombX + 1][bombY + 1].setText("1");
+                        }
+                    }
                 }
             }
         }
-        for (Bombs bomb : bombList){
+        for (Bombs bomb : bombList) {
             buttons[bomb.getX()][bomb.getY()].setText("");
         }
-
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
