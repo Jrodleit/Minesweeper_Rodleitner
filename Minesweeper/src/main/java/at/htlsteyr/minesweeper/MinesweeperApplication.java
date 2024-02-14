dcdf63d78cf448bb872f0fbbb7c7fa5871d73568
jrodleit <jrodleit@htl-steyr.ac.at>
2024-02-14 16:27:40 +0100

    winGame added, Method comments added, setNumbers fixed, Flag fixed, hard difficulty fixed and design added

--- Minesweeper/src/main/java/at/htlsteyr/minesweeper/MinesweeperApplication.java
+++ Minesweeper/src/main/java/at/htlsteyr/minesweeper/MinesweeperApplication.java
@@ -11,7 +11,7 @@ public class MinesweeperApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MinesweeperApplication.class.getResource("Minesweeper-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 1020, 640);
         stage.setTitle("Minesweeper");
         stage.setScene(scene);
         stage.show();
